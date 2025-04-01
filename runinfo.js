b2f611cba01688cf5b31b6a62aed0ac0b8e6a2fc
Chiara Bondi <cbondi@hamilton.edu>
2025-04-01 12:21:46 -0400

    Initial commit

---
+++ runinfo.js
@@ -0,0 +1,13 @@
+{
+    "name": “Monday Track Attack”,
+    "date": "2025-03-31",
+    "time": "06:30 PM",
+    "location": "https://www.google.com/maps/place/1+Overlook+Dr,+Pittsburgh,+PA+15217/@40.4329152,-79.9506021,17z/data=!3m1!4b1!4m6!3m5!1s0x8834f18c4738fac5:0xf6010c3eb7cac401!8m2!3d40.4329112!4d-79.9457312!16s%2Fg%2F11j8sfr9ql?entry=ttu&g_ep=EgoyMDI1MDMyNS4xIKXMDSoASAFQAw%3D%3D",
+    "distance_options": ["5K", "10K", "Half Marathon"],
+    "club": {
+        "name": “City Of Bridges Run Club”,
+        "contact_email": "cityofbridgesrunclub@gmail.com",
+        "website": "https://www.cityofbridgesrunclub.com/",
+    },
+    "registration_link": "https://www.cityofbridgesrunclub.com/event-details/track-attack-20250331",
+}
